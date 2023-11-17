404d7561c8672eb61160a270476dc5550fee4c01
root <smarttranfer@gmail.com>
2023-11-17 15:01:47 +0000

    feat: update file

---
+++ build_app_script.py
@@ -0,0 +1,20 @@
+import requests
+import argparse
+
+
+parser = argparse.ArgumentParser(description="Just an example", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-u", "--url", help="url_git")
+parser.add_argument("-f", "--file", help="path_file ")
+parser.add_argument("-p", "--url_api", help="url_api")
+args = parser.parse_args()
+
+url = f"{args.url_api}"
+payload = {'id': f'{args.url}'}
+files=[('file',('Facebook.apk',open(f"{args.file}",'rb'),'application/octet-stream'))]
+headers = {}
+response = requests.request("POST", url, headers=headers, data=payload, files=files)
+print(response.text)
+
+
+
+
